aeb5b17a96506ae350b0a67975c73b96b385c60e
juankBregar <juankbregar@gmail.com>
2018-11-15 11:38:52 -0600

    Added sessions and session storage. TODO OTB: integrate behavior for permissions and dinamyc Menu Ready to merge

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Dependencies
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const hbs = require('express-handlebars');
@@ -9,6 +10,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const validator = require('express-validator');
+const mongoStore = require('connect-mongo')(session);
 const { isLoggedin } = require('./config/middlewares');
 
 //Init express
@@ -40,8 +42,15 @@ require('./config/passport');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
+app.use(cookieParser());
 //Use express sessions
-app.use(session({ secret: 'test123', resave: false, saveUninitialized: false }));
+app.use(session({
+    secret: 'test123',
+    resave: false,
+    saveUninitialized: false,
+    store: new mongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 120 * 60 * 100 } //Dos horas
+}));
 //Use flash to store messages on session
 app.use(flash());
 //Init passport for handle users operation
@@ -49,8 +58,21 @@ app.use(passport.initialize());
 //Enable passport to use sessions
 app.use(passport.session());
 
-//Routes
+//Static dir
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
+/**
+ * Create global variables to use in views
+ */
+
+app.use((req, res, next) => {
+    //res.locals.allowed = 'Define middle ware that retrieves allowed menus'
+    res.locals.session = req.session;
+    next();
+})
+
+//Routes
 /**
  * Enable this routes after finishing inventory features.
  */
@@ -63,10 +85,6 @@ const orderRoute = require('./routes/orders');
 app.use('/ventas', isLoggedin, orderRoute);
 const userRoute = require('./routes/users');
 app.use('/users', userRoute);
-
-//Static dir
-app.use('/assets', express.static(__dirname + '/assets'));
-app.use('/uploads', express.static(__dirname + '/uploads'));
 
 //Index
 const indexRoute = require('./routes/index');
